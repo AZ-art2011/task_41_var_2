953f3ba95a27fa3029662f123fe6bdceaa916f96
Alexander <mypost@example.com>
2023-01-21 19:46:35 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41. Короткий вариант.
+Console.WriteLine("Вводите числа. 0 - завершение ввода");
+int temp = 0;
+int val = 1;
+
+while (val != 0)
+{
+    val = Convert.ToInt32(Console.ReadLine());
+    if (val > 0) temp++;
+}
+
+Console.WriteLine($"Количество чисел > 0 = {temp}");
